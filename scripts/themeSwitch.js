d0beb7c0534c995d2a8fe8a56bf443ed319c7235
Mac <mac@MacBook-Szymon.local>
2022-12-18 14:30:59 +0100

    Adding files to repository

---
+++ scripts/themeSwitch.js
@@ -0,0 +1,12 @@
+const switchTheme = () => {
+    const rootElem = document.documentElement;
+    let dataTheme = rootElem.getAttribute("data-theme"),
+        newTheme;
+    newTheme = dataTheme === "dark" ? "light" : "dark";
+
+    rootElem.setAttribute("data-theme", newTheme);
+
+    localStorage.setItem("theme", newTheme);
+};
+
+document.querySelector(".themeSwitcher").addEventListener("click", switchTheme);
